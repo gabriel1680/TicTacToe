0a30da3af606befd990179310386649f47c69072
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-08-10 23:55:55 -0300

    test: add isWin test

--- src/test/java/com/tictactoe/GameTest.java
+++ src/test/java/com/tictactoe/GameTest.java
@@ -31,4 +31,27 @@ class GameTest {
         assertEquals(Player.X, game.getBoard().get(1, 0));
         assertEquals(Player.O, game.whoseTurn());
     }
+
+    @Test
+    void isWin() {
+        game.takeTurn(0, 0);
+        assertFalse(game.isWin(Player.O));
+        assertFalse(game.isWin(Player.X));
+
+        game.takeTurn(1, 0);
+        assertFalse(game.isWin(Player.O));
+        assertFalse(game.isWin(Player.X));
+
+        game.takeTurn(0, 1);
+        assertFalse(game.isWin(Player.O));
+        assertFalse(game.isWin(Player.X));
+
+        game.takeTurn(1, 1);
+        assertFalse(game.isWin(Player.O));
+        assertFalse(game.isWin(Player.X));
+
+        game.takeTurn(0, 2);
+        assertTrue(game.isWin(Player.O));
+        assertFalse(game.isWin(Player.X));
+    }
 }
